60cc31c7e57da6538ba5cd4f32276d0e4cfa3177
tlimbach <49364223+tlimbach@users.noreply.github.com>
2025-06-01 18:49:57 +0200

    develop

--- src/main/java/ui/ThumbnailPanel.java
+++ src/main/java/ui/ThumbnailPanel.java
@@ -8,6 +8,7 @@ import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.io.File;
 import java.util.List;
+import java.util.concurrent.CompletableFuture;
 
 public class ThumbnailPanel extends JPanel {
 
@@ -26,45 +27,51 @@ public class ThumbnailPanel extends JPanel {
         add(scrollPane, BorderLayout.CENTER);
     }
 
+    int thumbnailsLoadedCount = 0;
+
     public void populate(List<File> mediaFiles) {
-        System.out.println("POpulating "+ mediaFiles.size() + " images");
+        System.out.println("Populating " + mediaFiles.size() + " files");
         gridPanel.removeAll();
 
         for (File file : mediaFiles) {
-            try {
-                if (Controller.isImageFile(file)) {
+            if (Controller.isImageFile(file)) {
+                // Bilder sofort synchron laden (geht schnell)
                 ImageIcon icon = new ImageIcon(file.getAbsolutePath());
                 Image scaled = getScaledImagePreserveRatio(icon.getImage(), THUMBNAIL_SIZE, THUMBNAIL_SIZE);
-                JLabel label = new JLabel(new ImageIcon(scaled));
-                label.addMouseListener(new MouseAdapter() {
-                    @Override
-                    public void mouseClicked(MouseEvent e) {
-                        Controller.getInstance().handleMedia(file);
+                addThumbnailLabel(scaled, file);
+                thumbnailsLoadedCount++;
+                Controller.getInstance().setThumbnailsLoaded(thumbnailsLoadedCount, mediaFiles.size());
+            } else if (Controller.isVideoFile(file)) {
+                // Videos asynchron laden
+                CompletableFuture.supplyAsync(() -> extractVideoThumbnail(file), Controller.getInstance().getExecutor()).thenAccept(thumbFile -> {
+                    thumbnailsLoadedCount++;
+                    if (thumbFile != null && thumbFile.exists()) {
+                        ImageIcon icon = new ImageIcon(thumbFile.getAbsolutePath());
+                        Image scaled = getScaledImagePreserveRatio(icon.getImage(), THUMBNAIL_SIZE, THUMBNAIL_SIZE);
+                        SwingUtilities.invokeLater(() -> {
+                            addThumbnailLabel(scaled, file);
+                        });
+                        Controller.getInstance().setThumbnailsLoaded(thumbnailsLoadedCount, mediaFiles.size());
                     }
                 });
-                gridPanel.add(label);}
-                if (Controller.isVideoFile(file)) {
-                    File thumbnail = extractVideoThumbnail(file);
-                    if (thumbnail != null && thumbnail.exists()) {
-                        ImageIcon icon = new ImageIcon(thumbnail.getAbsolutePath());
-                        Image scaled = getScaledImagePreserveRatio(icon.getImage(), THUMBNAIL_SIZE, THUMBNAIL_SIZE);
-                        JLabel label = new JLabel(new ImageIcon(scaled));
-                        label.addMouseListener(new MouseAdapter() {
-                            @Override
-                            public void mouseClicked(MouseEvent e) {
-                                Controller.getInstance().handleMedia(file);
-                            }
-                        });
-                        gridPanel.add(label);
-                    }
-                }
-            } catch (Exception e) {
-                e.printStackTrace();
             }
         }
 
-        gridPanel.revalidate();
-        gridPanel.repaint();
+      //  gridPanel.revalidate();
+      //  gridPanel.repaint();
+    }
+
+    private void addThumbnailLabel(Image image, File file) {
+        JLabel label = new JLabel(new ImageIcon(image));
+        label.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                Controller.getInstance().handleMedia(file);
+            }
+        });
+        gridPanel.add(label);
+        //gridPanel.revalidate();
+        //gridPanel.repaint();
     }
 
     private Image getScaledImagePreserveRatio(Image srcImg, int maxWidth, int maxHeight) {
@@ -86,13 +93,7 @@ public class ThumbnailPanel extends JPanel {
     private File extractVideoThumbnail(File videoFile) {
         try {
             File thumb = File.createTempFile("thumb_", ".png");
-            String[] cmd = {
-                    "ffmpeg", "-y",
-                    "-i", videoFile.getAbsolutePath(),
-                    "-ss", "00:00:10.000",
-                    "-vframes", "1",
-                    thumb.getAbsolutePath()
-            };
+            String[] cmd = {"ffmpeg", "-y", "-i", videoFile.getAbsolutePath(), "-ss", "00:00:10.000", "-vframes", "1", thumb.getAbsolutePath()};
             ProcessBuilder pb = new ProcessBuilder(cmd);
             pb.inheritIO().start().waitFor();
             return thumb;
